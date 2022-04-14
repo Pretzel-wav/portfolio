b085253be38bbca78cd940af7e18cf0f98978eb9
Justin Macak <justin.macak@gmail.com>
2022-04-14 18:25:59 -0400

    tried to add YT selector

--- better-recommendations/main.js
+++ better-recommendations/main.js
@@ -5,3 +5,8 @@ for (elt of images){
     elt.src = `${browser.runtime.getURL("./img/pretzo.jpg")}`
     elt.alt = 'an alt text'
 }
+
+var ytRecommended = document.getElementsByTagName('ytd-compact-video-renderer');
+for (video of ytRecommended){
+    video.class = 'School-in-the-summer';
+}
